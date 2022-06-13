91f8b1b7dd1303a47fe9ad1c7e4c7ba905d86f04
Tung <tungch@b0be834eb873.ant.amazon.com>
2022-06-13 17:31:47 +0700

    finalize 3: modify build-all.sh change context project

--- build-all.sh
+++ build-all.sh
@@ -22,7 +22,7 @@ IMAGE_TAG=latest
 echo "Trigerring openshift webhook"
 oclogin=`aws secretsmanager get-secret-value --secret-id ROSA/login-cmd --output=text | head -1 | cut -f4 -d'"'`
 $oclogin
-oc config set-context --current --namespace=tungch-voting-app
+oc config set-context --current --namespace=rosa-voting-app
 buildConfig="vote"
 secret_name=`oc get bc $buildConfig -o jsonpath='{.spec.triggers[?(@.generic)].generic.secretReference.name}'`
 webhookSecretKey=`oc get secret $secret_name -o jsonpath='{.data.WebHookSecretKey}' | base64 -d`
