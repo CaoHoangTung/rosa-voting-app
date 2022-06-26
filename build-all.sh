594f9e9d01ce394cc8b81a51925e03e3bbeb242e
CaoHoangTung <caohoangtung2001@gmail.com>
2022-06-26 22:33:49 +0700

    Update build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -1,24 +1,5 @@
 IMAGE_TAG=latest
 
-
-# docker build -t $IMAGE_REPO_NAME_VOTE ./voting-app/vote
-# docker build -t $IMAGE_REPO_NAME_RESULT ./voting-app/result
-# docker build -t $IMAGE_REPO_NAME_WORKER ./voting-app/worker
-
-# echo "Build done"
-
-# docker tag $IMAGE_REPO_NAME_VOTE:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME_VOTE:$IMAGE_TAG
-# docker tag $IMAGE_REPO_NAME_RESULT:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME_RESULT:$IMAGE_TAG
-# docker tag $IMAGE_REPO_NAME_WORKER:$IMAGE_TAG $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME_WORKER:$IMAGE_TAG
-
-# echo "Tag done"
-
-# docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME_VOTE:$IMAGE_TAG
-# docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME_RESULT:$IMAGE_TAG
-# docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME_WORKER:$IMAGE_TAG
-
-# echo "Pushed to ECR"
-
 echo "Trigerring openshift webhook"
 oclogin=`aws secretsmanager get-secret-value --secret-id ROSA/login-cmd --output=text | head -1 | cut -f4 -d'"'`
 $oclogin
